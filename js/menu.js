ce8bbd9dd7f4195f7e694790564f38ea82dc7bcc
Szymon-the-Cat <94720468+Szymon-the-Cat@users.noreply.github.com>
2022-01-27 17:26:33 +0100

    ddd

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,15 @@ const scrolling = document.querySelector(".main-header");
 window.addEventListener("scroll", function () {
   scrolling.classList.toggle("scrolling-active", window.scrollY > 80);
 });
+
+const btnMobileMenu = document.querySelector(".btn-mobile-nav");
+const mobileIcons = document.querySelectorAll(".btn-mobile-nav__icon");
+const mainMenu = document.querySelector(".menu");
+
+btnMobileMenu.addEventListener("click", function () {
+  mainMenu.classList.toggle("active");
+  mobileIcons.forEach((item) => {
+    item.classList.toggle("hidden");
+  });
+  btnMobileMenu.classList.toggle("btnn");
+});
